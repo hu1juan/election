035b027af2564582425eda6a3a3c29c1f8954fea
hu1juan <johnjeffritz@gmail.com>
2017-08-23 11:24:17 +0800

    master

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		templateUrl: 'html/login.html',
+		controller: 'loginCtrl'
+	})
+});
---
+++ js/controllers/controller.js
@@ -0,0 +1,5 @@
+'use strict'
+
+app.controller('loginCtrl',['$scope',function($scope){
+
+}]);
