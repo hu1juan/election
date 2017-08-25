0e2bfebee8f0d075333cf1ca4bc8b7d24fb8029e
hu1juan <johnjeffritz@gmail.com>
2017-08-25 13:37:52 +0800

    new login na pd

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,7 +1,26 @@
 'use strict'
 
-app.controller('loginCtrl',['$scope',function($scope){
+app.controller('loginCtrl',['$scope','$uibModal',function($scope,$uibModal){
+	$scope.open = function(size){
+		$uibModal.open({
+			animation: true,
+			templateUrl: 'modal/loginmodal.html',
+			controller: 'feedbackModalCtrl',
+			windowClass: 'app-modal-window',
+			size: size,
+		}).result.then(function(){},function(res){})
+	};
+}]);
 
+app.controller('feedbackModalCtrl',['$uibModalInstance','$scope','$location', function($uibModalInstance,$scope,$location){
+	$scope.ok = function(){
+		$uibModalInstance.dismiss();
+		$location.path('/admin');
+	}
+
+	$scope.cancel = function(){
+		$uibModalInstance.dismiss();
+	}
 }]);
 
 app.controller('adminCtrl',['$scope', function($scope){
